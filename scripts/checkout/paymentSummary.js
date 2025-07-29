766fd164518fdeff16b54e25fa3132bc4d67307e
caltron-xo <rajdeepsingha100@gmail.com>
2025-07-29 21:50:19 +0530

    Added totalQuantity in payment summary by myself

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -6,6 +6,7 @@ import { formatCurrency } from '../utils/money.js';
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let totalQuantity = 0; //added by me
 
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
@@ -13,6 +14,7 @@ export function renderPaymentSummary() {
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
+    totalQuantity += cartItem.quantity; //added by me
   });
 
   const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
@@ -23,7 +25,7 @@ export function renderPaymentSummary() {
     <div class="payment-summary-title">Order Summary</div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${totalQuantity}):</div>
             <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
           </div>
 
